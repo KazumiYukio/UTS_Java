1f849d120983aeb94a25bfad64b39b8fe5b4f6dd
KazumiYukio <kanieseiya61@gmail.com>
2020-09-08 12:14:04 +0700

    UTS_Java

---
+++ UTS.java
@@ -0,0 +1,65 @@
+package for_test;
+
+public class UTS {
+
+	public static void main(String[] args) {
+		
+		//alfabet
+		String huruf = "ABCDEFGHIJKLMNOPQRSTUVWXYZ#-";
+		
+		//pengambilan huruf nama abdulloh
+		char a = huruf.charAt(0);
+		char b = huruf.charAt(1);
+		char d = huruf.charAt(3);
+		char u = huruf.charAt(20);
+		char l = huruf.charAt(11);
+		char o = huruf.charAt(14);
+		char h = huruf.charAt(7);
+		
+		//pengambilan huruf nama mahmud
+		char m = huruf.charAt(12);
+		
+		//pengambilan huruf nama jalaludin
+		char j = huruf.charAt(9);
+		char i = huruf.charAt(8);
+		char n = huruf.charAt(13);
+		
+		//charater
+		char spasi = huruf.charAt(27);
+		
+		
+		//pemanggilan nama abdulloh
+		System.out.print(a);
+		System.out.print(b);
+		System.out.print(d);
+		System.out.print(u);
+		System.out.print(l);
+		System.out.print(l);
+		System.out.print(o);
+		System.out.print(h);
+		
+		System.out.print(spasi);
+		
+		//pemanggilan nama mahmud
+		System.out.print(m);
+		System.out.print(a);
+		System.out.print(h);
+		System.out.print(m);
+		System.out.print(u);
+		System.out.print(d);
+		
+		System.out.print(spasi);
+		
+		//pemanggilan nama jalaludin
+		System.out.print(j);
+		System.out.print(a);
+		System.out.print(l);
+		System.out.print(a);
+		System.out.print(l);
+		System.out.print(u);
+		System.out.print(d);
+		System.out.print(i);
+		System.out.print(n);
+	}
+
+}
